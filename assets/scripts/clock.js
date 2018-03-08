35e14b32d5a23dd1f5e4ced7a967130f888a709a
Wynn Blevins <wynnblevins@yahoo.com>
2018-03-08 17:06:17 -0500

    Demo commit.

---
+++ assets/scripts/clock.js
@@ -0,0 +1,23 @@
+var clock = (function () {
+    var clockObj = {};
+
+    clockObj.getRemainingTime = function (endtime){
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor( (t/1000) % 60 );
+        var minutes = Math.floor( (t/1000/60) % 60 );
+        var hours = Math.floor( (t/(1000*60*60)) % 24 );
+        var days = Math.floor( t/(1000*60*60*24) );
+        
+        // making a change
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    return clockObj;
+})();
+
